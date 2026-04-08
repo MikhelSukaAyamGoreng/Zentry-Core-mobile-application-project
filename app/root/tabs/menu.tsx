5329a6865a353708a4459d3e90c65d6dc0502d39
MikhelSukaAyamGoreng <mikhelessien3@gmail.com>
2026-04-08 22:42:46 +0700

    first commit

---
+++ app/root/tabs/menu.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Text, View, Image } from 'react-native'
+import styles from "../properties/stylesheet"
+import TopNavBar from './top_NavBar'
+
+export class menu extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TopNavBar />
+        <View style={styles.menu_container}>
+          <View style={styles.menu_card}>
+            <View style={styles.menu_image_container}>
+              <Image source={require("../../../assets/icons/Home.png")} style={styles.menu_image}/>
+            </View>
+            <Text style={styles.menu_texts}> Living Room </Text>
+          </View>
+          <View style={styles.menu_card}>
+            <View style={styles.menu_image_container}>
+              <Image source={require("../../../assets/icons/Bedroom.png")} style={styles.menu_image}/>
+            </View>
+            <Text style={styles.menu_texts}> Bed Room </Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default menu
